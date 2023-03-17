6f6b7eaeff514523396dc2c3864f921192c0d970
Hongrui Fang <chfanghr@gmail.com>
2023-03-18 02:00:34 +0800

    add aeson instances for `TreasuryWithdrawalDatum`

--- src/Agora/Effect/TreasuryWithdrawal.purs
+++ src/Agora/Effect/TreasuryWithdrawal.purs
@@ -3,9 +3,12 @@ module Agora.Effect.TreasuryWithdrawal where
 
 import Prelude
 
+import Aeson (class DecodeAeson, class EncodeAeson)
 import Contract.Credential (Credential)
 import Contract.PlutusData (class FromData, class ToData)
 import Contract.Value (Value)
+import Ctl.Extra.FieldOrder (class FieldOrder)
+import Ctl.Extra.IsData (productFromData, productToData)
 import Data.Generic.Rep (class Generic)
 import Data.Lens (Iso')
 import Data.Lens.Iso.Newtype (_Newtype)
@@ -13,8 +16,6 @@ import Data.Newtype (class Newtype)
 import Data.Tuple (Tuple)
 import Data.Tuple.Nested (type (/\))
 import Prim.RowList (Cons, Nil)
-import Ctl.Extra.FieldOrder (class FieldOrder)
-import Ctl.Extra.IsData (productFromData, productToData)
 
 newtype TreasuryWithdrawalDatum = TreasuryWithdrawalDatum
   { receivers :: Array (Credential /\ Value)
@@ -38,6 +39,10 @@ instance FromData TreasuryWithdrawalDatum where
   fromData = productFromData
 
 derive newtype instance Show TreasuryWithdrawalDatum
+
+derive newtype instance EncodeAeson TreasuryWithdrawalDatum
+
+derive newtype instance DecodeAeson TreasuryWithdrawalDatum
 
 --------------------------------------------------------------------------------
 
